023dbea84dfb5efc6840c2918813e0fcce5bcaa5
0xPsilo <szostek.bartek@gmail.com>
2014-10-23 00:00:09 +0200

    Added primitive random text builder class

---
+++ src/pl/polsl/java/Bartlomiej/Szostek/models/RandomTextModel.java
@@ -0,0 +1,72 @@
+package pl.polsl.java.Bartlomiej.Szostek.models;
+
+import java.lang.StringBuilder;
+import java.util.Random;
+
+/**
+ *
+ * @author Bartek
+ */
+public class RandomTextModel {
+    private final int maxWordLen;
+    private final int numOfWords;
+
+    public RandomTextModel() {
+        this.maxWordLen = 10;
+        this.numOfWords = 40;
+    }
+
+    public RandomTextModel(int maxWordLength, int numberOfWords) {
+        this.maxWordLen = maxWordLength;
+        this.numOfWords = numberOfWords;
+    }
+    
+    public String GenerateText() {
+        StringBuilder textBuilder = new StringBuilder();
+        Random random = new Random();
+        
+        for (int i = 0; i < numOfWords; i++) {
+            textBuilder.append(getRandomWord(random, i == (numOfWords - 1)));
+        }
+        
+        return textBuilder.toString();
+    }
+    
+    private String getRandomWord(Random r, boolean last) {
+        char sign;
+        StringBuilder word = new StringBuilder(maxWordLen + 2);
+        int len = r.nextInt(maxWordLen) + 1;
+        
+        for (int i = 0; i < len; i++) {     
+            if ((i == 0) && (r.nextInt(100) > 80)) {
+                sign = (char) (r.nextInt(26) + 'a');
+            }
+            else {
+                sign = (char) (r.nextInt(26) + 'A');
+            }
+            word.append(sign);
+        }
+        
+        sign = (char) r.nextInt(100);
+        if (last == true) {
+            word.append('.');
+        }
+        else if (sign > 10) {
+            word.append("! ");
+        }
+        else if (sign > 20) {
+            word.append("? ");
+        }
+        else if (sign > 40) {
+            word.append(". ");
+        }
+        else if (sign > 50) {
+            word.append(", ");
+        }
+        else {
+            word.append(' ');
+        }
+        
+        return word.toString();
+    }
+}
--- src/pl/polsl/java/Bartlomiej/Szostek/models/TextModel.java
+++ src/pl/polsl/java/Bartlomiej/Szostek/models/TextModel.java
@@ -10,10 +10,28 @@ package pl.polsl.java.Bartlomiej.Szostek.models;
  * @author Bartek
  */
 public class TextModel {
-    private final String[] userInputText;
-
+    
+    private int currentIndex;
+    private String userInputText;
+    private String templateText;
+    
     public TextModel() {
         this.userInputText = null;
+        this.templateText = null;
+        this.currentIndex = 0;
+    }
+
+    public TextModel(String templateText) {
+        this.templateText = templateText;
+        this.currentIndex = 0;
     }
     
+    public boolean addSignToText(char sign) {
+        ++currentIndex;
+        userInputText += sign;
+        
+        return (userInputText.charAt(currentIndex) 
+                == templateText.charAt(currentIndex)) ? true
+                                                      : false;
+    }
 }
